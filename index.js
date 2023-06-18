87c0fa5ba15a7486f987796488ca37eb7ce61a80
Soquel <128098051+lesquel@users.noreply.github.com>
2023-06-17 22:40:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-
+let index = 0,
+sliders = document.querySelectorAll(".slider");
+document.addEventListener("click", e=>{
+    if (!e.target.matches(".btn_p") && !e.target.matches(".btn_n")) return false;
+    if (e.target.matches(".btn_n")) {
+        index < 0 || index >= sliders.length -1 ? index = 0 : index++;
+    };
+    if (e.target.matches(".btn_p")) {
+        index <= 0  ? index = sliders.length - 1 : index--;
+    };
+    
+    sliders.forEach(el =>{
+        el.classList.add("desactivo")
+    })
+    sliders[index].classList.remove("desactivo")
+    
+}, false)
